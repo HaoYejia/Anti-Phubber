2ad8f2abd6062759692ac48be9b159f5baa02e61
HaoYejia <41829200+HaoYejia@users.noreply.github.com>
2019-04-24 17:07:21 +0800

    Update to a usable version

--- Codes/dataCollection/dataCollection.ino
+++ Codes/dataCollection/dataCollection.ino
@@ -1,11 +1,15 @@
-// Edit by Yejia Hao
+// Create by Yejia Hao
 // 20190420
 
 #include <DFRobot_BMI160.h>
 
 DFRobot_BMI160 bmi160;
 const int8_t i2c_addr = 0x69;
+const int buttonPin = 10; //Define number of pin of the button
+int loopCount = 0; //A counter for the number of loops
+
 void setup(){
+  pinMode(buttonPin,INPUT);
   Serial.begin(115200);
   delay(100);
   
@@ -25,32 +29,53 @@ void setup(){
 void loop(){  
   int i = 0;
   int rslt;
-  int16_t accelGyro[6]={0}; 
+  int16_t accelGyro[6]={0}; //cpp文件533行，规定传入参数格式为int16_t
+  float accelRaw [3] = {0};float gyroRaw [3] = {0}; //问题可能出在数据格式
+//  float accelDelta [3] = {0};float gyroDelta [3] = {0};
+//  float accelCorrected [3] = {0}; float gyroCorrected [3] = {0};
   
   //get both accel and gyro data from bmi160
   //parameter accelGyro is the pointer to store the data
   rslt = bmi160.getAccelGyroData(accelGyro); //wtf?
   
   if(rslt == 0){
-    Serial.print(millis());Serial.print("\t"); //Serial print the timestamp, in milliseconds
-//    for(i=0;i<6;i++){
-//      Serial.print 
-//    }
-    for(i=0;i<6;i++){
-      if (i<3){
-        //the first three are gyro datas
-        Serial.print(accelGyro[i]*3.14/180.0);Serial.print("\t");
+//    Serial.print(millis());Serial.print("\t"); //Serial print the timestamp, in milliseconds
+    
+    // Check the state of button and print it 
+//    if(digitalRead(buttonPin)==HIGH){
+//      Serial.print("OFF");Serial.print("\t"); 
+//   }
+//    if(digitalRead(buttonPin)==LOW){
+//      Serial.print("ON");Serial.print("\t"); //Check Apr 20th,2019 in the journal for the reason why HIGH for off and LOW for on
+//   }
+
+       gyroRaw[0]=accelGyro[0]*3.14/180.0;
+       gyroRaw[1]=accelGyro[1]*3.14/180.0;
+       gyroRaw[2]=accelGyro[2]*3.14/180.0;
+       accelRaw[0] = accelGyro[3]/16384.0;
+       accelRaw[1] = accelGyro[4]/16384.0;
+       accelRaw[2] = accelGyro[5]/16384.0;
+
+       
+
+//print test of my version of output
+       Serial.print(gyroRaw[0]); Serial.print("\t");
+       Serial.print(gyroRaw[1]); Serial.print("\t");
+       Serial.print(gyroRaw[2]); Serial.print("\t");
+       Serial.print(accelRaw[0]);Serial.print("\t");
+       Serial.print(accelRaw[1]);Serial.print("\t");
+       Serial.print(accelRaw[2]);Serial.print("\t");
+
+      Serial.println();
       }
-      else{
-        //the following three data are accel datas
-        Serial.print(accelGyro[i]/16384.0);Serial.print("\t");
-      }
-    }
-    Serial.println();
-  }else{
+
+   else{
     Serial.println("err");
   }
+
   delay(100);
+
+  loopCount = loopCount +1;
   /*
    * //only read accel data from bmi160
    * int16_t onlyAccel[3]={0};
